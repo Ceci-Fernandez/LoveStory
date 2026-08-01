31e9a7223f3d82ba17a6c4ee8a23dbd531f9bcae
Cecilia <cecilia92fernandez@gmail.com>
2026-08-01 00:13:36 -0300

    se cambio credenciales

--- config/database.php
+++ config/database.php
@@ -4,11 +4,16 @@
  * Ajustar credenciales según el entorno.
  */
 
-define('DB_HOST', 'localhost');
+/*define('DB_HOST', 'localhost');
 define('DB_NAME', 'lovestory');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8mb4');*/
+define('DB_HOST', 'sql110.infinityfree.com');
+define('DB_NAME', 'if0_42550079_lovestory');
+define('DB_USER', 'if0_42550079');
+define('DB_PASS', 'Monkimin92');
+define('DB_CHARSET', 'utf8mb4'); 
 
 function getConnection(): PDO
 {
